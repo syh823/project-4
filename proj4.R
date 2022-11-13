171284049cb626a7be72248ac295ec91c212d427
fahimahghazali <fahimahghazali@icloud.com>
2022-11-13 16:41:03 +0000

    approximate hessian matrix function, pass arguments, store grad(theta)

--- proj4.R
+++ proj4.R
@@ -1,34 +1,44 @@
 # separate function for finite difference
+# separate function for finite difference
+Hfd = function(theta, grad, eps, ...){
+  grad0 <- grad(theta,...)
+  Hfd <- matrix(0,length(theta),length(theta)) ## finite diference Hessian 
+  for (i in 1:length(theta)) { ## loop over parameters
+    th1 <- theta; th1[i] <- th1[i] + eps ## increase theta by eps 
+    grad1 <- grad(th1,...) ## compute resulting grad at th1
+    Hfd[i,] <- (grad1 - grad0)/eps ## approximate second derivs by row
+  }
+  return(Hfd)
+}
+
 
 newt = function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,
                 maxit=100,max.half=20,eps=1e-6){
 
   # original = func(theta)
-  original=func(theta)
+  original = func(theta, ...)
   
   # grad (theta)
-  origin_grad=grad(theta)
+  grad_vec = grad(theta, ...)
   
   # if is.nan(original) == T or is.nan(grad(theta)) == T: stop(the objective or derivatives are not finite at the initial theta)
-  if(is.nan(original) == T | sum(is.nan(origin_grad))!=0)
+  if(is.nan(original) == T | sum(is.nan(grad_vec))!=0)
     stop("the objective or derivatives are not finite at the initial theta")
 
-  # if hess not provided, find hess
-  if(is.null(hess)==TRUE){
-    # use finite difference page 72 notes
-    hess=XXXX
-  }
+
 
   # maxit = 0
   maxit=0
   
   # while grad not absolute value less than tol times 
-  while(sum(abs(grad(theta))>tol)!=0 & maxit <= 100){ # the absolute value of the objective function plus fscale 
+  while(sum(abs(grad_vec)>tol)!=0 & maxit <= 100){ # the absolute value of the objective function plus fscale 
     # maxit = maxit + 1
     maxit=maxit+1
-    
+    # if hess not provided, find hess
+    if(is.null(hess)==TRUE) hess_mat = Hfd(theta = theta, grad = grad, eps = eps, ...)
+    else hess_mat = hess(theta)
     # check if hess if positive definite using chol
-    flag=try (chol(hess(theta)))
+    flag=try (chol(hess_mat))
 
     # if not 
     if(class(flag)[1]=='try-error'){
@@ -36,10 +46,10 @@ newt = function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,
     }
     
     # inverse of hess use chol and backsolve
-    inverse_hess=solve(hess(theta))
+    inverse_hess = solve(hess_mat)
     
     # delta = -(hess(theta))^(-1)*grad(theta)
-    delta = -inverse_hess%*%grad(theta)
+    delta = -inverse_hess%*%grad_vec
     
     # newtheta = theta + delta
     newtheta=theta+delta
@@ -51,7 +61,7 @@ newt = function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,
     max.half=0
     
     # while new > original and max.half <= 20
-    while(new>original& max.half<=20){
+    while(new > original& max.half<=20){
       # max.half = max.half + 1
       max.half=max.half+1
       
@@ -59,26 +69,27 @@ newt = function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,
       newtheta= theta+ delta/2
       # new = func(newtheta)
       
-      new=func(newtheta)
+      new = func(newtheta)
     }
     # if max.half > 20 stop("step fails to reduce the objective despite trying max.half step halvings")
     if(max.half > 20)
       stop("step fails to reduce the objective despite trying max.half step halvings")
     # grad(newtheta)
-    theta=newtheta
+    theta = newtheta
+    grad_vec = grad(theta)
   }
   # if maxit > 100 stop()
   if(maxit>100)
     stop("fail")
 
   # if hess(theta) not posdef warning("hess matrix not posdef")
-  if(class(try (chol(hess(theta))))[1]=='try-error')
+  if(class(try (chol(hess_mat)))[1]=='try-error')
     warning("hess matrix not posdef")
   print(theta)
   print(maxit)
 }
 
-newt(c(-2.3,-3.9),rb,gb,hb)
+
 
 
 
